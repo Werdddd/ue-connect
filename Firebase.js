0572cae27e84e69862eb6c25e145db1f196f13ca
guiribajustin2004 <127721330+Sheeeenn@users.noreply.github.com>
2025-04-24 22:49:55 +0800

    [Done] Firebase

---
+++ Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnbswPJogi8BHEemJ5dj-38KChGAzJFnM",
+  authDomain: "ue-connect.firebaseapp.com",
+  projectId: "ue-connect",
+  storageBucket: "ue-connect.firebasestorage.app",
+  messagingSenderId: "761554615717",
+  appId: "1:761554615717:web:2d1597a661824a8d577c4c"
+};
+
+const app = initializeApp(firebaseConfig);
