c0e1b9f9d52bc0137b9bf710f5b653eeacbe4703
keni <kennyranes2319@gmail.com>
2025-04-29 01:10:17 +0800

    [fix]

--- components/organizationCard.js
+++ components/organizationCard.js
@@ -3,44 +3,13 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 import { firestore } from '../Firebase';
 import { doc, getDoc } from 'firebase/firestore';
 
-export default function OrganizationCard() {
-
-    const [logo, setLogo] = useState(null);
-    const [memberCount, setMemberCount] = useState(0);
-    const [description, setDescription] = useState('');
-    const [orgName, setOrgName] = useState('');
-
-    useEffect (() => {
-
-        const fetchOrgData = async () =>{
-            try{
-                const docRef = doc(firestore, 'organizations', 'orgId') ;
-                const docSnap = await getDoc(docRef);
-
-                //get values
-                if (docSnap.exists()) {
-                    const data = docSnap.data();
-                    setOrgName(data.orgName);
-                    setMemberCount(data.members);
-                    setDescription(data.description);
-                    setLogo(data.logoUrl);
-                }else{
-                    console.log('No Registry Found')
-                }
-                
-            }catch(error){
-                console.error('Error fetching organzation data:', error)
-            }
-        }
-
-        fetchOrgData();
-    }, []);
+export default function OrganizationCard({ orgName, memberCount, description, logo }) {
 
     return (
         <View style={styles.card}>
             <View style={styles.headerRow}>
                 <Image
-                    source={{ uri: logo }}
+                    source={logo}
                     style={styles.orgcardlogo}
                 />
                 <Text style={styles.orgName}>{orgName}</Text>
