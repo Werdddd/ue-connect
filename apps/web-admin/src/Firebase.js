dd16a89e42c3e5b74f56abaadd88d22112894964
keni <kennyranes2319@gmail.com>
2025-09-27 21:52:58 +0800

    firebase install

---
+++ apps/web-admin/src/Firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnbswPJogi8BHEemJ5dj-38KChGAzJFnM",
+    authDomain: "ue-connect.firebaseapp.com",
+    projectId: "ue-connect",
+    storageBucket: "ue-connect.firebasestorage.app",
+    messagingSenderId: "761554615717",
+    appId: "1:761554615717:web:2d1597a661824a8d577c4c"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore };
